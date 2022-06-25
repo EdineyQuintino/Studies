cbcbdf68f88e72e9917981f7b5188e908c82ec9d
EdineyQuintino <ediney56@outlook.com>
2022-06-25 16:28:36 -0300

    Realizado atualização

--- Salary.js
+++ Salary.js
@@ -1,4 +1,4 @@
-/*const salario = [
+const salario = [
     {
       valor: 10, 
       horas: 160
@@ -16,6 +16,6 @@
   const imposto = 27;
   salario.forEach((salario, index) => { 
     console.log(`Employer ${index+1} : seu salario será de ${salario.valor*salario.horas-imposto} `)
-    });*/
+    });
     
     
